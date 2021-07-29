d553cedfb212baffefb0ca2de19a35e8983b6d72
yuu <spare.fran626bel@gmail.com>
2021-07-29 07:00:42 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,10 +50,10 @@ class TasksController < ApplicationController
         redirect_to tasks_url
         
     end
-end
 
-private
+    private
 
-def task_params
+    def task_params
     params.require(:task).permit(:content)
+    end
 end
